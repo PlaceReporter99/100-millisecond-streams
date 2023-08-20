fda4a6d7e1ef02b8a048c0aa42e0f53bbacd88dd
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-20 09:28:05 +0100

    Update write_data.py

--- write_data.py
+++ write_data.py
@@ -4,7 +4,7 @@ commands = \
 f"""
 mkdir random_data
 cd random_data
-timeout -s SIGKILL 1m cat /dev/urandom >> {secrets.token_hex(128)}.txt
+timeout -s SIGKILL 1m cat /dev/urandom >> {secrets.token_hex(64)}.txt
 cd ..
 """
 os.system(commands)
